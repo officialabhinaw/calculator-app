9ea04ca3ca740c6ef245fd7253914537573fdc5b
Abhinaw kumar <officialabhinaw790@gmail.com>
2026-09-10 23:22:00 +0530

    Initial commit - Calculator App

---
+++ script.js
@@ -0,0 +1,32 @@
+const display=document.getElementById('display');
+    const boxes=document.querySelectorAll('.box');
+    boxes.forEach(box =>
+    {
+        box.addEventListener('click',()=>
+    {
+        const value =box.textContent.trim();
+        if(value==="=")
+        {
+            try
+            {
+                display.value=eval(display.value);
+            }
+            catch(error)
+            {
+               display.value="Error";
+            }
+        }
+        else if(value==="AC")
+        {
+           display.value="";
+        }
+        else if(value==="back")
+        {
+            display.value=display.value.slice(0 ,-1);
+        }
+        else
+        {
+            display.value+=value;
+        }
+    });
+    });
