856aca8027a552cdf20d0a01f9ccfcc117e33b2f
Abhinaw kumar <officialabhinaw790@gmail.com>
2026-09-11 21:48:51 +0530

    update

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-const display=document.getElementById('display');
-    const boxes=document.querySelectorAll('.box');
-    boxes.forEach(box =>
-    {
-        box.addEventListener('click',()=>
-    {
-        const value =box.textContent.trim();
-        if(value==="=")
-        {
-            try
-            {
-                display.value=eval(display.value);
+const display = document.getElementById('display');
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const value = box.textContent.trim();
+        if (value === "=") {
+            try {
+                display.value = eval(display.value);
             }
-            catch(error)
-            {
-               display.value="Error";
+            catch (error) {
+                display.value = "Error";
             }
         }
-        else if(value==="AC")
-        {
-           display.value="";
+        else if (value === "AC") {
+            display.value = "";
         }
-        else if(value==="back")
-        {
-            display.value=display.value.slice(0 ,-1);
+        else if (value === "back") {
+            display.value = display.value.slice(0, -1);
         }
-        else if(value==="%")
-        {
-           display.value=display.value.slice(1/100);
+        else if (value === "%") {
+            display.value = display.value.slice(1 / 100);
         }
-        else
-        {
-            display.value+=value;
+        else {
+            display.value += value;
         }
     });
-    });
+});
+document.getElementById('output').addEventListener('input',(e)=>
+{
+    console.log(e.target.innertext);
+});
